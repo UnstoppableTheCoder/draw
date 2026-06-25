3eaff44d257afd120ee48aaed6aee0ba83e2794e
vipendra <vipendrasinghpatel999@gmail.com>
2026-06-25 22:16:18 +0530

    Create usePointerHelpers hook

--- apps/web/features/editor/hooks/use-canvas-interactions.ts
+++ apps/web/features/editor/hooks/use-canvas-interactions.ts
@@ -3,14 +3,11 @@ import useCanvasContext from "./use-canvas-context";
 import useDrawing from "./use-drawing";
 import useSelectionInteraction from "./use-selection-interaction";
 import usePan from "./use-pan";
-import { getScreenToCanvasCoordinates } from "../utils/get-coordinates";
 import * as store from "../store/selectors";
 import { usePointerState } from "./use-pointer-state";
-import { ToolType } from "@/types/toolbar.types";
-import { getCanvasCursor } from "../utils/get-canvas-cursor";
 import useCanvasEraser from "./use-canvas-eraser";
 import useCanvasRenderer from "./use-canvas-renderer";
-import { isLocalURL } from "next/dist/shared/lib/router/utils/is-local-url";
+import usePointerHelpers from "./use-pointer-helpers";
 
 export default function useCanvasInteractions(
   canvasRef: RefObject<HTMLCanvasElement | null>,
@@ -18,10 +15,7 @@ export default function useCanvasInteractions(
 ) {
   const scale = store.useScale();
   const panOffset = store.usePanOffset();
-  const scaleOffset = store.useScaleOffset();
   const selectedTool = store.useSelectedTool();
-  const setSelectedShape = store.useSetSelectedShape();
-  const setSelectedShapeBounds = store.useSetSelectedShapeBounds();
   const setTextEditingState = store.useSetTextEditingState();
   const setSelectedTool = store.useSetSelectedTool();
   const selectedShape = store.useSelectedShape();
@@ -45,88 +39,20 @@ export default function useCanvasInteractions(
   const eraser = useCanvasEraser(ctxRef, canvasRef);
   useCanvasRenderer(ctxRef);
 
-  // Sets the required initial states
-  function initializePointerState(event: PointerEvent<HTMLCanvasElement>) {
-    const canvas = event.currentTarget;
-    canvas.setPointerCapture(event.pointerId);
-    pointerRefs.isPointerDownRef.current = true;
-
-    const point = getScreenToCanvasCoordinates({
-      screenX: event.clientX,
-      screenY: event.clientY,
-      canvas,
-      panOffset,
-      scale,
-      scaleOffset,
-    });
-
-    pointerRefs.startPointRef.current = point;
-    pointerRefs.lastPointerRef.current = point;
-    return point;
-  }
-
-  // Sets the required initial states for Middle Mouse Pan
-  function handleMiddleMousePan(event: PointerEvent<HTMLCanvasElement>) {
-    if (event.button !== 1) return false;
-
-    pointerRefs.isPanningRef.current = true;
-    pointerRefs.panStartMouseRef.current = {
-      x: event.clientX,
-      y: event.clientY,
-    };
-
-    pointerRefs.panStartOffsetRef.current = { ...panOffset };
-    return true;
-  }
-
-  // Get Canvas Point
-  function getCurrentCanvasPoint(e: React.PointerEvent<HTMLCanvasElement>) {
-    return getScreenToCanvasCoordinates({
-      screenX: e.clientX,
-      screenY: e.clientY,
-      canvas: e.currentTarget,
-      panOffset,
-      scale,
-      scaleOffset,
-    });
-  }
-
-  // Get Delta & Update Last Point
-  function getPointerDelta(endPoint: any) {
-    const last = pointerRefs.lastPointerRef.current;
-    if (!last) return null;
-
-    const dx = endPoint.x - last.x;
-    const dy = endPoint.y - last.y;
-
-    pointerRefs.lastPointerRef.current = endPoint;
-    return { dx, dy };
-  }
-
-  // Clear Selection
-  function clearSelection() {
-    setSelectedShape(null);
-    setSelectedShapeBounds(null);
-  }
-
-  function updateCanvasCursor(tool: ToolType, isPanning: boolean) {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-    canvas.style.cursor = getCanvasCursor(tool, isPanning);
-  }
+  const pointerHelpers = usePointerHelpers(canvasRef, pointerRefs);
 
   // ============== DOM Pointer Events Handlers ==============
   function handlePointerDown(event: PointerEvent<HTMLCanvasElement>) {
     event.preventDefault();
 
     // clear selection if needed
-    clearSelection();
+    pointerHelpers.clearSelection();
 
     // Sets the required initial states for Middle Mouse Pan
-    if (handleMiddleMousePan(event)) return;
+    if (pointerHelpers.handleMiddleMousePan(event)) return;
 
     // Sets the required initial states
-    const startPoint = initializePointerState(event);
+    const startPoint = pointerHelpers.initializePointerState(event);
 
     // Route based on tool
     if (selectedTool === "pan") {
@@ -136,7 +62,10 @@ export default function useCanvasInteractions(
       };
       pointerRefs.panStartOffsetRef.current = { ...panOffset };
       pointerRefs.isPanningRef.current = true;
-      updateCanvasCursor(selectedTool, pointerRefs.isPanningRef.current);
+      pointerHelpers.updateCanvasCursor(
+        selectedTool,
+        pointerRefs.isPanningRef.current,
+      );
       return;
     }
 
@@ -159,7 +88,7 @@ export default function useCanvasInteractions(
     const isPointerDown = pointerRefs.isPointerDownRef.current;
     const isPanning = pointerRefs.isPanningRef.current;
 
-    updateCanvasCursor(selectedTool, isPanning);
+    pointerHelpers.updateCanvasCursor(selectedTool, isPanning);
 
     if (selectedTool === "text") {
       return;
@@ -170,10 +99,10 @@ export default function useCanvasInteractions(
       return;
     }
 
-    const endPoint = getCurrentCanvasPoint(event);
+    const endPoint = pointerHelpers.getCurrentCanvasPoint(event);
 
     // update last pointer for delta calculations
-    const delta = getPointerDelta(endPoint);
+    const delta = pointerHelpers.getPointerDelta(endPoint);
     if (!delta) return;
 
     if (selectedTool === "select") {
@@ -195,20 +124,14 @@ export default function useCanvasInteractions(
     drawing.onPointerMoveDrawing(event);
   }
 
-  function resetPointerState() {
-    pointerRefs.isPanningRef.current = false;
-    pointerRefs.isPointerDownRef.current = false;
-    pointerRefs.resizableHandleRef.current = null;
-    pointerRefs.resizeStartBoundsRef.current = null;
-    pointerRefs.resizeStartFontSizeRef.current = null;
-    pointerRefs.lineResizeStateRef.current = null;
-  }
-
   function handlePointerUp(event: PointerEvent<HTMLCanvasElement>) {
     const isResizing = pointerRefs.isResizingRef.current;
-    updateCanvasCursor(selectedTool, pointerRefs.isPanningRef.current);
+    pointerHelpers.updateCanvasCursor(
+      selectedTool,
+      pointerRefs.isPanningRef.current,
+    );
 
-    resetPointerState();
+    pointerHelpers.resetPointerState();
 
     if (selectedShape && selectedShape.type === "text") {
       const isDragging = pointerRefs.isDraggingRef.current;
@@ -226,7 +149,7 @@ export default function useCanvasInteractions(
         });
 
         // Clear Selection if shape is being edited
-        clearSelection();
+        pointerHelpers.clearSelection();
       }
 
       pointerRefs.pointerDownTimeRef.current = null;
--- apps/web/features/editor/hooks/use-pointer-helpers.ts
+++ apps/web/features/editor/hooks/use-pointer-helpers.ts
@@ -1,13 +1,98 @@
-export default function usePointerHelpers() {
-  function initializePointerState() {}
+import { PointerEvent, RefObject } from "react";
+import { usePointerState } from "./use-pointer-state";
+import { getScreenToCanvasCoordinates } from "../utils/get-coordinates";
+import * as store from "../store/selectors";
+import { ToolType } from "@/types/toolbar.types";
+import { getCanvasCursor } from "../utils/get-canvas-cursor";
 
-  function handleMiddleMousePan() {}
+export default function usePointerHelpers(
+  canvasRef: RefObject<HTMLCanvasElement | null>,
+  pointerRefs: ReturnType<typeof usePointerState>,
+) {
+  const panOffset = store.usePanOffset();
+  const scale = store.useScale();
+  const scaleOffset = store.useScaleOffset();
+  const setSelectedShape = store.useSetSelectedShape();
+  const setSelectedShapeBounds = store.useSetSelectedShapeBounds();
 
-  function getCurrentCanvasPoint() {}
+  // Sets the required initial states
+  function initializePointerState(event: PointerEvent<HTMLCanvasElement>) {
+    const canvas = event.currentTarget;
+    canvas.setPointerCapture(event.pointerId);
+    pointerRefs.isPointerDownRef.current = true;
 
-  function getPointerDelta() {}
+    const point = getScreenToCanvasCoordinates({
+      screenX: event.clientX,
+      screenY: event.clientY,
+      canvas,
+      panOffset,
+      scale,
+      scaleOffset,
+    });
 
-  function resetPointerState() {}
+    pointerRefs.startPointRef.current = point;
+    pointerRefs.lastPointerRef.current = point;
+    return point;
+  }
+
+  // Sets the required initial states for Middle Mouse Pan
+  function handleMiddleMousePan(event: PointerEvent<HTMLCanvasElement>) {
+    if (event.button !== 1) return false;
+
+    pointerRefs.isPanningRef.current = true;
+    pointerRefs.panStartMouseRef.current = {
+      x: event.clientX,
+      y: event.clientY,
+    };
+
+    pointerRefs.panStartOffsetRef.current = { ...panOffset };
+    return true;
+  }
+
+  // Get Canvas Point
+  function getCurrentCanvasPoint(e: React.PointerEvent<HTMLCanvasElement>) {
+    return getScreenToCanvasCoordinates({
+      screenX: e.clientX,
+      screenY: e.clientY,
+      canvas: e.currentTarget,
+      panOffset,
+      scale,
+      scaleOffset,
+    });
+  }
+
+  // Get Delta & Update Last Point
+  function getPointerDelta(endPoint: any) {
+    const last = pointerRefs.lastPointerRef.current;
+    if (!last) return null;
+
+    const dx = endPoint.x - last.x;
+    const dy = endPoint.y - last.y;
+
+    pointerRefs.lastPointerRef.current = endPoint;
+    return { dx, dy };
+  }
+
+  function resetPointerState() {
+    pointerRefs.isPanningRef.current = false;
+    pointerRefs.isPointerDownRef.current = false;
+    pointerRefs.resizableHandleRef.current = null;
+    pointerRefs.resizeStartBoundsRef.current = null;
+    pointerRefs.resizeStartFontSizeRef.current = null;
+    pointerRefs.lineResizeStateRef.current = null;
+  }
+
+  function updateCanvasCursor(tool: ToolType, isPanning: boolean) {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    canvas.style.cursor = getCanvasCursor(tool, isPanning);
+  }
+
+  // Clear Selection
+  function clearSelection() {
+    setSelectedShape(null);
+    setSelectedShapeBounds(null);
+  }
 
   return {
     initializePointerState,
@@ -15,5 +100,7 @@ export default function usePointerHelpers() {
     getCurrentCanvasPoint,
     getPointerDelta,
     resetPointerState,
+    updateCanvasCursor,
+    clearSelection,
   };
 }
